f3db8333d49b2e2e5809671b8ff59e27a8cebd35
Calvin Mathews <calvinriyadh@gmail.com>
2024-08-28 15:41:35 +0530

    Initial

---
+++ Automate_text.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import time
+import urllib.parse
+
+chrome_options = Options()
+chrome_options.binary_location = 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe'
+chrome_options.add_argument('--user-data-dir=C:/Users/Calvin\'s Acer/AppData/Local/BraveSoftware/Brave-Browser/User Data')
+chrome_options.add_argument('--profile-directory=Default')
+
+service = Service('C:\\Users\\Calvin\'s Acer\\Downloads\\chromedriver-win64\\chromedriver.exe')
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get('https://web.whatsapp.com')
+
+time.sleep(10)
+
+df = pd.read_excel('contacts.xlsx')
+
+for index, row in df.iterrows():
+    name = row['Name']
+    number = row['Phone']
+    
+    message = f"Thank you {name}, for confirming your presence at Converge 2024! 🎉 \n\nKindly pray and prepare yourselves for the event 🙏 \n\nLook out for more details from us here on WhatsApp. And if you have any queries, do reply here. \n\nWe look forward to meeting you! \n\n*Talmid House*"
+    
+    try:
+        encoded_message = urllib.parse.quote(message)
+        chat_url = f"https://web.whatsapp.com/send?phone={number}&text={encoded_message}"
+        driver.get(chat_url)
+        time.sleep(2)
+        send_button = WebDriverWait(driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, '//span[@data-icon="send"]'))
+        )
+        
+        send_button.click()
+        
+        print(f"Message sent to {name} ({number})")
+        
+    except Exception as e:
+        print(f"Failed to send message to {name} ({number}): {e}")
+    
+    time.sleep(5)
+
+driver.quit()
